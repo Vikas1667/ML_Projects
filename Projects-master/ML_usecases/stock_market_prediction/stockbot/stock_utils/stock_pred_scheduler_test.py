d2dcc77296dbb88f7a46954a9ecb2e59d153e9a9
Vikas Kumbharkar <vikas.kumbharkar308@gmail.com>
2024-03-23 18:55:09 +0530

    stock prediction pipeline updated

---
+++ Projects-master/ML_usecases/stock_market_prediction/stockbot/stock_utils/stock_pred_scheduler_test.py
@@ -0,0 +1,167 @@
+import pandas as pd
+import pandas as pd
+import yfinance as yf
+import matplotlib.pyplot as plt
+import streamlit as st
+from nselib import capital_market
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import schedule
+import os,sys
+import pandas as pd
+import datetime
+import yfinance as yf
+import schedule,time
+sys.path.append('..')
+from lstm_model import model_train_triger
+import streamlit as st
+import os,sys
+# sys.path.append('..')
+# from stock_data_extract_scheduler import lstm_model_scheduler
+
+
+import tensorflow
+import datetime
+
+## nse site data
+## can be further improvise with selenium for weekly updating list of stocks and
+# download latest by using click link https://pythonexamples.org/python-selenium-click-a-link/
+
+model_path=r'../data/stock_metadata/models/lstm_fin_nifty_prediction_01012015to02032024.keras'
+
+
+
+
+dir_path=r"V:/ML_projects/StockMarket_Analysis/data/stock_metadata/"
+file_path="NIFTY FINANCIAL SERVICES_Historical_PR_01012015to02032024.csv"
+
+def equity_history(symbol,start_date,end_date):
+    try:
+        # start_date=date_range_string[0].strftime("%d-%m-%Y")
+        # end_date=date_range_string[1].strftime("%d-%m-%Y")
+
+        # data = capital_market.price_volume_and_deliverable_position_data(symbol=symbol, from_date=start_date,to_date=end_date)
+
+        data = yf.download(symbol, start_date, end_date)
+        # data.head()
+
+        return data
+
+    except Exception as e:
+        return e
+
+def data_read():
+
+    # df=pd.read_csv(os.path.join(dir_path,file_path))
+    date_range_string=datetime.datetime.now() - datetime.timedelta(days=180000), datetime.datetime.now()
+
+    # data=yahoo_index_data("NIFTY_FIN_SERVICE.NS")
+    # data=data.rename_axis('DateTime').reset_index()
+    # plot_chart(data)
+
+    df = equity_history("NIFTY_FIN_SERVICE.NS", date_range_string[0], date_range_string[1])
+
+    data = df.rename_axis('DateTime').reset_index()
+
+    return data
+
+def lstm_model_scheduler(df):
+
+    ### from previous checkpoint retraining should be trigger
+    # st.write("Scheduler started")
+
+    print("scheduler started")
+    model=model_train_triger(df)
+    model.save('..\data\stock_metadata\models/lstm_fin_nifty_prediction_latest1.keras')
+
+def load_lstm_model(model_path):
+    model = tensorflow.keras.saving.load_model(model_path)
+    return model
+
+def read_data(stck_path):
+    nse_list=pd.read_csv(stck_path)
+    # st.table(nse_list)
+
+
+def equity_history(symbol,start_date,end_date):
+    try:
+        # start_date=date_range_string[0].strftime("%d-%m-%Y")
+        # end_date=date_range_string[1].strftime("%d-%m-%Y")
+
+        # data = capital_market.price_volume_and_deliverable_position_data(symbol=symbol, from_date=start_date,to_date=end_date)
+
+        data = yf.download(symbol, start_date, end_date)
+        # data.head()
+
+        return data
+
+    except Exception as e:
+        # return st.write(e)
+        return e
+
+def yahoo_index_data(symbol):
+    '''
+
+    :param symbol: nse symbol
+    :return: data download
+
+    '''
+    data = yf.download(tickers=symbol, period='1d', interval='15m')
+    return data
+
+def plot_chart(data):
+    fig = go.Figure()
+    fig.add_trace(
+        go.Candlestick(x=data['DateTime'], open=data['Open'], high=data['High'], low=data['Low'], close=data['Close']))
+
+    st.plotly_chart(fig)
+
+if __name__ == '__main__':
+    # st.title("Stock Data Predictions")
+    # index=st.selectbox("historical data",['Nifty',"Nifty Financial","Bank Nifty"])
+    date_range_string =datetime.datetime.now() - datetime.timedelta(days=18000), datetime.datetime.now()
+
+    # data=yahoo_index_data("NIFTY_FIN_SERVICE.NS")
+    # data=data.rename_axis('DateTime').reset_index()
+    # plot_chart(data)
+    # st.write(date_range_string[0],date_range_string[1])
+
+    df=equity_history("NIFTY_FIN_SERVICE.NS",date_range_string[0],date_range_string[1])
+
+    data = df.rename_axis('DateTime').reset_index()
+
+    # plot_chart(data)
+    #
+    # model=load_lstm_model(model_path)
+    # st.table(data_read().head(10))
+    # if st.button("Prediction"):
+    #     model_pred(model,data)
+    #
+    # if st.sidebar.button("schedule"):
+    #     try:
+    #         schedule.every().day.at("23:47").do(lstm_model_scheduler,df=df)
+    #     except Exception as e:
+    #         st.write(e)
+    try:
+        # schedule.every().day.at("00:42").do(lstm_model_scheduler, df=data)
+        lstm_model_scheduler(df)
+    except Exception as e:
+        print("Exception occured as schedule")
+
+
+    # while True:
+    #     # Checks whether a scheduled task
+    #     # is pending to run or not
+    #     schedule.run_pending()
+    #     print("schedule is in running")
+    #     time.sleep(1)
+
+    # st.write(df)
+# data_path = "../data/stock_metadata/ind_nifty50list.csv"
+
+
+# read_data(data_path)
+
+
+#if prediction for close as well as for all other
+
---
+++ Projects-master/ML_usecases/stock_market_prediction/stockbot/utils.py
@@ -0,0 +1,124 @@
+import imaplib
+import email
+import yaml
+import pandas as pd
+import streamlit as st
+
+
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import traceback
+import streamlit as st
+
+
+
+with open('V:\ML_projects\StockMarket_Analysis\stockbot\pages\cred_data\gmail_cred.yml') as f:
+     content = f.read()
+
+my_credentials = yaml.load(content, Loader = yaml.FullLoader)
+user, password = my_credentials["user"], my_credentials['password']
+imap_url = 'imap.gmail.com'
+my_mail = imaplib.IMAP4_SSL(imap_url)
+my_mail.login(user, password)
+my_mail.select('Inbox')
+
+data = my_mail.search(None, 'ALL')
+mail_ids = data[1]
+
+
+def email_extractor():
+    id_list = mail_ids[0].split()
+    len(id_list)#This should be equal to the total number of emails you have seen above
+    first_email_id = int(id_list[0])
+    latest_email_id = int(id_list[-1])
+
+
+    email_df = pd.DataFrame(columns=['Date','From', 'Subject','Status'], index=range(100000,first_email_id,-1))
+
+    for i in range(100000, first_email_id, -1):
+        data = my_mail.fetch(str(i), '(RFC822)')
+        for response_part in data:
+            arr = response_part[0]
+            if isinstance(arr, tuple):
+                msg = email.message_from_string(str(arr[1], 'ISO-8859–1'))
+                print('msg',i)  # This will let you know what row is being appended
+                new_row = pd.Series({"Date": msg['Date'], "From": msg['from'], "Subject": msg['subject'],
+                                     "Status": msg['X-Antivirus-Status']})
+    email_df = email_df.append(new_row, ignore_index=True)
+    st.Dataframe(email_df)
+
+
+################# NEws EXtractor
+
+
+def item_search(item, limit, page):
+    stocknews = f"https://www.thestar.com.my/search/?q={item}&qsort=oldest&qrec={limit}&qstockcode=&pgno={page}"
+
+    html = requests.get(stocknews).text
+    # print(html)
+    # st.write(html)
+
+    soup = BeautifulSoup(html, 'html.parser')
+    st.write(soup)
+    return soup
+
+
+def get_details(url):
+    html = requests.get(url).text
+    st.write(html)
+    soup = BeautifulSoup(html, 'html.parser')
+    st.write(soup)
+    content = soup.find('div', {'id': 'story-body'})
+    st.write(content)
+    if content:
+        content.get_text(strip=True)
+        return content
+    else:
+        return ""
+
+
+def star_new_crawler(page, search_query, limit):
+    df=pd.DataFrame()
+    title = []
+    links = []
+    premium = []
+    new_type = []
+    contents = []
+    publishedDate = []
+    while True:
+        print(page)
+        try:
+            result = item_search(search_query, limit, page)
+            title += [x.get_text(strip=True) for x in result.find_all("h2", {"class": "f18"})]
+            links += [x.find('a', {"data-content-type": "Article"})['href'] for x in
+                      result.find_all("h2", {"class": "f18"})]
+            premium += [x.get_text(strip=True) for x in result.find_all("span", {"class": "biz-icon"})]
+            new_type += [x.get_text(strip=True) for x in result.find_all("a", {"class": "kicker"})]
+            contents += [get_details(x) for x in links]
+            publishedDate += [x.get_text(strip=True) for x in result.find_all("span", {"class": "timestamp"})]
+
+            if len(title) == 0:
+                break
+        except Exception as e:
+            print(e)
+            traceback.print_exc()
+
+        page += 1
+
+    df=pd.DataFrame({'new_type': new_type, 'title': title, 'premium': premium, 'links': links, 'published_data': publishedDate,
+         'contents': contents})
+
+    df.to_excel(f'../data/{search_query}.xlsx', index=False)
+
+    # if len(df)>0:
+    #     df.to_csv('../data/'+str(search_query) +'news.csv')
+
+    return df
+# if __name__ == '__main__':
+#     page = 1
+#     search_query = 'TCS'
+#     limit = 30
+#     star_new_crawler(page, search_query, limit)
+
+###
